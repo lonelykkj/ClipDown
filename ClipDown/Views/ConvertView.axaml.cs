bb75703b7eac3df6bdfd7e164cac380795f3707d
Heitor Carvalho <heitormanoel05@gmail.com>
2025-11-21 21:47:09 -0300

    adiciona a nova tela ConvertView

--- ClipDown/Views/ConvertView.axaml.cs
+++ ClipDown/Views/ConvertView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace ClipDown.Views;
@@ -9,5 +10,10 @@ public partial class ConvertView : UserControl
     public ConvertView()
     {
         InitializeComponent();
+    }
+    
+    public void GoToMenu(object? sender, RoutedEventArgs args)
+    {
+        this.Content = new Views.MainWindow();
     }
 }
